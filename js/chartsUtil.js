124cfc234715ca1ccd78a19c8c00a4916eda115c
BillyLuo <luotao2013@126.com>
2016-10-10 17:48:50 +0800

    '10.10'

--- js/chartsUtil.js
+++ js/chartsUtil.js
@@ -1,5 +1,15 @@
 //version:0.1
-//图表类型(bar(柱形),line(折线),pie(饼图),scatter(散点图),map(地图),radar(雷达图),graph(关系图),funnel(漏斗图),gauge(仪表盘图),heatmap(热力图),candlestick(k线图));
+//description:
+	//图表类型(bar(柱形),line(折线),pie(饼图),scatter(散点图),map(地图),radar(雷达图),graph(关系图),funnel(漏斗图),gauge(仪表盘图),heatmap(热力图),candlestick(k线图));
+	//自定义的方法参数说明：
+	//var myChart = iViz.line(container,option,theme);
+	//1.必须参数，container(element，图表的容器)，option(object，图表数据核心对象)
+	//2.可选参数   theme(string，图表主题，使用时必须先引入相应theme的js文件)
+	
+	//图表方法说明：
+	//1.方法中的所有参数都是可以改动的，但必须保证一些必须的默认参数
+	//2.想要更复杂的图表，只需要根据相应的格式设置option对象就可以。
+	//3.方法可以自行添加
 (function () {
 	//图表核心js(iVizCharts.js)，必须在这之前引入
 	var iViz = iVizCharts;
@@ -10,13 +20,21 @@
 		//主标题和副标题之间的距离，单位是px
 		itemGap:6
 	}
+	//工具栏
 	var myToolBox = {
 		show:true,
 		orient:'horizontal',
 		left:40,
 		feature:{
+			//自动转化类型，折线、柱形、折叠、平铺
 			magicType:{
 				type:['line','bar','stack','tiled']
+			},
+			restore:{
+				
+			},
+			dataView:{
+				
 			}
 		}
 	}
@@ -36,26 +54,17 @@
 			return 'middle';
 		}
 	}
-	//自定义的方法参数说明：
-	//1.必须参数，container(element，图表的容器)，option(object，图表数据核心对象)
-	//2.可选参数   theme(string，图表主题，使用时必须先引入相应theme的js文件)
-	
-	//图表方法说明：
-	//1.方法中的所有参数都是可以改动的，但必须保证一些必须的默认参数
-	//2.想要更复杂的图表，只需要根据相应的格式设置option对象就可以。
-	//3.方法可以自行添加
 	
 	//bar(柱状图)
 	iViz.bar = function (container,option,theme) {
 		var defaultOption = {
 			title:myTitle,
-			toolbox:myToolBox,
 			//图例
 			legend:{
 				data:[],
 				orient:'horizontal',
 				top:38,
-			    right:'5%'
+				right:'5%'
 			},
 		    tooltip : {
 		        trigger: 'axis',
@@ -80,7 +89,9 @@
 		    series : []
 		}
 		if(option.series){
-			option.legend = {};
+			if(!option.legend) {
+				option.legend = {};
+			}
 			option.legend.data = [];
 			var series = option.series;
 			for(var prop in series){
@@ -89,7 +100,9 @@
 				}else{
 					option.legend.data.push(series[prop]["name"]);
 				}
-				series[prop].type = 'bar'
+				if(!series[prop]['type']){
+					series[prop].type = 'bar'
+				}
 			}
 		}else{
 			console.log('can not find series')
@@ -99,8 +112,10 @@
 		if(option){
 			//初始化图表
 			myChart.setOption(defaultOption);
-			if(option.xAxis.name){
+			if(!option.xAxis.nameGap){
 				option.xAxis.nameGap = myxAxis.setxAxisNameGap(option.xAxis.name);
+			}
+			if(!option.xAxis.nameLocation){
 				option.xAxis.nameLocation = myxAxis.setxAxisNameLocation(option.xAxis.name);
 			}
 			//根据传入的数据刷新图表
@@ -117,7 +132,6 @@
 	iViz.line = function (container,option,theme) {
 			var defaultOption = {
 			title:myTitle,
-			toolbox:myToolBox,
 		    tooltip : {
 		        trigger: 'axis'
 		    },
@@ -150,7 +164,9 @@
 				}else{
 					option.legend.data.push(series[prop]["name"]);
 				}
-				series[prop].type = 'line'
+				if(!series[prop]['type']){
+					series[prop].type = 'line'
+				}
 			}
 		}else{
 			console.log('can not find series')
@@ -160,8 +176,10 @@
 		var myChart = iViz.init(container,myTheme);
 		if(option){
 			myChart.setOption(defaultOption);
-			if(option.xAxis.name){
+			if(!option.xAxis.nameGap){
 				option.xAxis.nameGap = myxAxis.setxAxisNameGap(option.xAxis.name);
+			}
+			if(!option.xAxis.nameLocation){
 				option.xAxis.nameLocation = myxAxis.setxAxisNameLocation(option.xAxis.name);
 			}
 			myChart.setOption(option);
@@ -177,7 +195,7 @@
 			title:myTitle,
 		    tooltip : {
 		    	trigger:'item',
-		        formatter: "{b} : {c} ({d}%)"
+		        formatter: "{c},({d}%)"
 		    },
 		    itemStyle: {
                 emphasis: {
@@ -198,13 +216,10 @@
 		    ]
 		}
 		if(option.series){
-			for(var prop in option.series){
-				option.series[prop].type = 'pie';
-				option.series[prop].label = {
-					normal:{
-						show:true,
-						formatter:'{b}  {d}%'
-					}
+			var series = option.series;
+			for(var prop in series){
+				if(!series[prop]['type']){
+					series[prop].type = 'pie'
 				}
 			}
 		}else {
@@ -226,7 +241,6 @@
 	iViz.scatter = function (container,option,theme) {
 		var defaultOption = {
 			title:myTitle,
-			grid: myGrid,
 		    tooltip : {
 		    	trigger:'item',
 		        formatter: "{c}"
@@ -260,8 +274,11 @@
 		    series : []
 		}
 		if(option.series){
-			for(var prop in option.series){
-				option.series[prop].type = 'scatter';
+			var series = option.series;
+			for(var prop in series){
+				if(!series[prop]['type']){
+					series[prop].type = 'scatter'
+				}
 			}
 		}else {
 			console.log('series can not be find');
@@ -296,18 +313,19 @@
 		    series : []
 		}
 		if(option.series){
-			for(var prop in option.series){
+			var series = option.series;
+			for(var prop in series){
 				//设置地图图表的类型
-				option.series[prop].type = 'map';
-				option.series[prop].map = 'china';
+				series[prop].type = 'map';
+				series[prop].map = 'china';
 				//设置是否可以缩放
-				option.series[prop].roam = true,
+				series[prop].roam = true,
 				//设置选择模式，可以是single、multiple
-				option.series[prop].selectedMode = 'multiple';
-				option.series[prop].scaleLimit = {
+				series[prop].selectedMode = 'multiple';
+				series[prop].scaleLimit = {
 					min:1
 				},
-				option.series[prop].label = {
+				series[prop].label = {
 	                normal: {
 	                    show: true
 	                },
@@ -315,7 +333,7 @@
 	                    show: true
 	                }
 	           	},
-	           	option.series[prop].itemStyle = {
+	           	series[prop].itemStyle = {
 	           		emphasis:{
 	           			borderColor:'#f00'
 	           		}
@@ -360,11 +378,13 @@
 				//设置series里面第一组数据的类型为effecScatter（散点图）；
 				series[prop].map = 'china';
 				//设置是否可以缩放
-				series[prop].roam = true,
+				series[prop].roam = true, 
 				series[prop].scaleLimit = {
 					min:1
 				}
-				series[0].type = 'effectScatter';
+				if(!series[prop]['type']){
+					series[prop].type = 'effectScatter'
+				}
 			}
 		}else {
 			console.log('series can not be find');
@@ -384,7 +404,6 @@
 	iViz.radar = function (container,option,theme) {
 		var defaultOption = {
 			title:myTitle,
-			grid: myGrid,
 		    tooltip : {
 		    	trigger:'item'
 		    },
@@ -398,8 +417,11 @@
 		    series : []
 		}
 		if(option.series){
-			for(var prop in option.series){
-				option.series[prop].type = 'radar';
+			var series = option.series;
+			for(var prop in series){
+				if(!series[prop]['type']){
+					series[prop].type = 'radar'
+				}
 			}
 		}else {
 			console.log('series can not be find');
@@ -422,8 +444,11 @@
 			series:[]
 		};
 		if(option.series) {
-			for(var prop in option.series){
-				option.series[prop].type = 'graph';
+			var series = option.series;
+			for(var prop in series){
+				if(!series[prop]['type']){
+					series[prop].type = 'graph'
+				}
 			}
 		}else{
 			console.log('can not find options')
@@ -449,8 +474,11 @@
 			series:[]
 		};
 		if(option.series) {
-			for(var prop in option.series){
-				option.series[prop].type = 'funnel';
+			var series = option.series;
+			for(var prop in series){
+				if(!series[prop]['type']){
+					series[prop].type = 'funnel'
+				}
 			}
 		}else{
 			console.log('can not find options')
@@ -461,7 +489,7 @@
 			myChart.setOption(defaultOption);
 			myChart.setOption(option);
 		}else {
-			console.log('erro!can not find option');
+			console.log('error!can not find option');
 		}
 		return myChart;
 	}
@@ -475,8 +503,11 @@
 			series:[]
 		};
 		if(option.series) {
-			for(var prop in option.series){
-				option.series[prop].type = 'gauge';
+			var series = option.series;
+			for(var prop in series){
+				if(!series[prop]['type']){
+					series[prop].type = 'gauge'
+				}
 			}
 		}else{
 			console.log('can not find option')
@@ -487,7 +518,7 @@
 			myChart.setOption(defaultOption);
 			myChart.setOption(option);
 		}else {
-			console.log('erro!can not find option');
+			console.log('error!can not find option');
 		}
 		return myChart;
 	}
@@ -499,8 +530,11 @@
 			series:[]
 		};
 		if(option.series) {
-			for(var seriesItem in option.series) {
-				option.series[seriesItem].type = 'heatmap';
+			var series = option.series;
+			for(var prop in series) {
+				if(!series[prop]['type']){
+					series[prop].type = 'heatmap'
+				}
 			}
 		}else {
 			console.log('warning!series did not exist.');
@@ -518,13 +552,16 @@
 		var defaultOption = {
 			series:[]
 		};
-//		if(option.series) {
-//			for(var seriesItem in option.series) {
-//				option.series[seriesItem].type = 'candlestick';
-//			}
-//		}else {
-//			console.log('warning!series did not exist.');
-//		}
+		if(option.series) {
+			var series = option.series;
+			for(var prop in series) {
+				if(!series[prop]['type']){
+					series[prop].type = 'candlestick'
+				}
+			}
+		}else {
+			console.log('warning!series did not exist.');
+		}
 		if(option) {
 			myChart.setOption(defaultOption);
 		}
@@ -540,8 +577,11 @@
 			series:[]
 		};
 		if(option.series) {
-			for(var seriesItem in option.series) {
-				option.series[seriesItem].type = 'treemap';
+			var series = option.series;
+			for(var prop in series) {
+				if(!series[prop]['type']){
+					series[prop].type = 'treemap'
+				}
 			}
 		}else {
 			console.log('warning!series did not exist.');
